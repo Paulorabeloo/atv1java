182224cd04cb7be7ad55b13f5275475ba6d3849b
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2024-04-07 12:54:42 -0300

    atvjava1

---
+++ Aula1.java
@@ -0,0 +1,29 @@
+package com.mycompany.aula1;
+
+public class Aula1 
+{
+
+    public static void main(String[] args) //classe principal
+    {
+    // Polegadas para centimetros
+    double pol = 3; 
+		
+    double cm = pol * 2.54;
+		
+    System.out.println("Resultado: " + cm);
+    
+    //Dolares para reais
+    double dol = 10;
+    
+    double reais = dol * 5.07;
+    
+    System.out.println("Resultado: " + reais);
+    
+    //Milhas para km
+    double milha = 50;
+    
+    double km = milha * 1.6;
+    
+    System.out.println("Resultado: " + km);
+    }
+}
