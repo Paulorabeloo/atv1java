4ddbaa83e8f2324175f8ec5f37fb7df90cdaae50
Paulo Rabelo <pauloeduardorabelomiranda@gmail.com>
2024-04-10 10:22:01 -0300

    Aula1.java

--- Aula1.java
+++ Aula1.java
@@ -5,25 +5,25 @@ public class Aula1
 
     public static void main(String[] args) //classe principal
     {
-    // Polegadas para centimetros
-    double pol = 3; 
+    	// Polegadas para centimetros
+    	double pol = 3; 
 		
-    double cm = pol * 2.54;
+    	double cm = pol * 2.54;
 		
-    System.out.println("Resultado: " + cm);
+    	System.out.println("Resultado: " + cm);
     
-    //Dolares para reais
-    double dol = 10;
+    	//Dolares para reais
+    	double dol = 10;
     
-    double reais = dol * 5.07;
+    	double reais = dol * 5.07;
     
-    System.out.println("Resultado: " + reais);
+    	System.out.println("Resultado: " + reais);
     
-    //Milhas para km
-    double milha = 50;
+   	 //Milhas para km
+    	double milha = 50;
     
-    double km = milha * 1.6;
+    	double km = milha * 1.6;
     
-    System.out.println("Resultado: " + km);
+    	System.out.println("Resultado: " + km);
     }
 }
